b4f317c39712391c91b61d0b11223d507c3d17bf
485byte <dungtk876@gmail.com>
2026-09-21 20:57:02 +0700

    Oh no

--- src/main/java/viridian/content/ViridianBlocks.java
+++ src/main/java/viridian/content/ViridianBlocks.java
@@ -2,14 +2,29 @@ package viridian.content;
 
 import mindustry.world.Block;
 import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.Items;
 
 public class ViridianBlocks {
 
     public static Block testBlock;
 
     public static void load() {
+
+        System.out.println("=== VIRIDIAN: BLOCK LOAD START ===");
+
         testBlock = new Block("viridian-test-block");
 
+        System.out.println(
+            "=== VIRIDIAN: BLOCK CREATED: " + testBlock.name + " ==="
+        );
+
         testBlock.category = Category.effect;
+
+        testBlock.requirements(
+            ItemStack.with(Items.copper, 1)
+        );
+
+        System.out.println("=== VIRIDIAN: BLOCK SETUP COMPLETE ===");
     }
 }
