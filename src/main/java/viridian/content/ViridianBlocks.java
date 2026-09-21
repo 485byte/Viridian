5bd65da846ac73a7fce2609bc482810791381b6f
485byte <dungtk876@gmail.com>
2026-09-21 21:05:37 +0700

    Hope it work

--- src/main/java/viridian/content/ViridianBlocks.java
+++ src/main/java/viridian/content/ViridianBlocks.java
@@ -2,8 +2,6 @@ package viridian.content;
 
 import mindustry.world.Block;
 import mindustry.type.Category;
-import mindustry.type.ItemStack;
-import mindustry.content.Items;
 
 public class ViridianBlocks {
 
@@ -20,10 +18,6 @@ public class ViridianBlocks {
         );
 
         testBlock.category = Category.effect;
-
-        testBlock.requirements(
-            ItemStack.with(Items.copper, 1)
-        );
 
         System.out.println("=== VIRIDIAN: BLOCK SETUP COMPLETE ===");
     }
