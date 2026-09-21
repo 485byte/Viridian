6f88d3380a52c076bd7f5af6d7529f0fb43419df
485byte <dungtk876@gmail.com>
2026-09-21 20:26:53 +0700

    Update ViridianMod.java

--- src/main/java/viridian/ViridianMod.java
+++ src/main/java/viridian/ViridianMod.java
@@ -1,10 +1,16 @@
 package viridian;
 
 import mindustry.mod.Mod;
+import viridian.content.ViridianBlocks;
 
 public class ViridianMod extends Mod {
 
     public ViridianMod() {
         System.out.println("Viridian Java loaded!");
+    }
+
+    @Override
+    public void loadContent() {
+        ViridianBlocks.load();
     }
 }
