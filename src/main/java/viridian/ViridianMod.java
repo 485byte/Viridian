f9cc25cb1373db95968cd4a043038b45630a1993
485byte <dungtk876@gmail.com>
2026-09-21 20:57:41 +0700

    Nope

--- src/main/java/viridian/ViridianMod.java
+++ src/main/java/viridian/ViridianMod.java
@@ -6,11 +6,15 @@ import viridian.content.ViridianBlocks;
 public class ViridianMod extends Mod {
 
     public ViridianMod() {
-        System.out.println("Viridian Java loaded!");
+        System.out.println("=== VIRIDIAN: MOD CONSTRUCTOR ===");
     }
 
     @Override
     public void loadContent() {
+        System.out.println("=== VIRIDIAN: LOAD CONTENT START ===");
+
         ViridianBlocks.load();
+
+        System.out.println("=== VIRIDIAN: LOAD CONTENT END ===");
     }
 }
