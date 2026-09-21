04c45486ec79fa159818d8d09918a97b02695dd9
485byte <dungtk876@gmail.com>
2026-09-21 20:13:31 +0700

    Add ViridianMod

---
+++ src/main/java/viridian/ViridianMod.java
@@ -0,0 +1,10 @@
+package viridian;
+
+import mindustry.mod.Mod;
+
+public class ViridianMod extends Mod {
+
+    public ViridianMod() {
+        System.out.println("Viridian Java loaded!");
+    }
+}
