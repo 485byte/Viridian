a359362ddb6e988dcf65fbdcb793bdd274f83b56
485byte <dungtk876@gmail.com>
2026-09-21 20:24:49 +0700

    Add ViridianBlocks

---
+++ src/main/java/viridian/content/ViridianBlocks.java
@@ -0,0 +1,12 @@
+package viridian.content;
+
+import mindustry.world.Block;
+
+public class ViridianBlocks {
+
+    public static Block testBlock;
+
+    public static void load() {
+        testBlock = new Block("viridian-test-block");
+    }
+}
